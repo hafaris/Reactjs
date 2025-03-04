34ef1eb5e8c3ee07860f600947b012742dea22a3
Faris Mohammed K C <farismhd954@gmail.com>
2025-03-04 11:51:08 +0530

    theme changer app
    light mode
    dark mode

---
+++ context-api-app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React, { useContext } from "react";
+import { ThemeContext, ThemeProvider } from "./ThemeContext";
+
+function ThemeSwitcher() {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  return (
+    <div className={`app ${theme}`}>
+      <p>Current Theme: {theme}</p>
+      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+        Toggle Theme
+      </button>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <ThemeSwitcher />
+    </ThemeProvider>
+  );
+}
